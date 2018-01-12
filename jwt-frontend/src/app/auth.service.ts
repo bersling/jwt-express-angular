0607a42b0101bbdfe37f66438cc42e9558c6a3ec
bersling <bersling@gmail.com>
2018-01-12 15:50:51 +0100

    JWT Part 1: Generating the token using the sign function

--- jwt-frontend/src/app/auth.service.ts
+++ jwt-frontend/src/app/auth.service.ts
@@ -8,14 +8,17 @@ export class AuthService {
 
   api = 'http://localhost:5000/api';
   loggedIn = new BehaviorSubject<boolean>(false);
+  token;
 
   login(email: string, password: string) {
     this.http.post(this.api + '/login', {
       email: email,
       password: password
     }).subscribe((resp: any) => {
-      this.loggedIn.next(resp);
-      this.toastr.success(resp ? `Welcome ${resp.name}` : 'Logged in!');
+      this.loggedIn.next(true);
+      this.token = resp.token;
+      console.log(this.token);
+      this.toastr.success(resp && resp.user && resp.user.name ? `Welcome ${resp.user.name}` : 'Logged in!');
     }, (errorResp) => {
       this.loggedIn.next(undefined);
       errorResp.error ? this.toastr.error(errorResp.error.errorMessage) : this.toastr.error('An unknown error has occured.');
